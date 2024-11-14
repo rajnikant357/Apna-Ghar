dd5334dcbe4d0b28ed2817bf5429cd249aead234
rajnikant357 <rajnikantg357@gmail.com>
2024-11-14 18:41:50 +0530

    location button 14-11-2024

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function hidesidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+const dropdown = document.getElementById('myDropdown');
+
+dropdown.addEventListener('change', function() {
+  const selectedValue = dropdown.value;
+  console.log('Selected value:', selectedValue);
+  // You can perform actions based on the selected value here
+});const dropdownBtn = document.querySelector('.dropdown-btn');
+const dropdownContent = document.querySelector('.dropdown-content');
+const locationList = document.getElementById('location-btn');
+function gotlocation(position){
+  console.log(position);
+}
+function failed(){
+  console.log("there was a problem..")
+}
+const button = document.getElementById('location-btn');
+button.addEventListener('click', async() =>{
+  const result = navigator.geolocation.getCurrentPosition(gotlocation, failed) 
+    
+  
+})
